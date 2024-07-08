c8976cf2d006971d0d66468810a5284f395fbac5
daa25209 <114696597+daa25209@users.noreply.github.com>
2024-07-08 08:12:30 +0200

    Update model.py

--- model.py
+++ model.py
@@ -4,9 +4,7 @@ from torchvision.models import resnet50 as resnet
 from torchvision.models import ResNet50_Weights as weights
 from typing import Callable
 
-
 class VisualOdometryModel(nn.Module):
-
     def __init__(
         self,
         hidden_size: int,
@@ -14,7 +12,6 @@ class VisualOdometryModel(nn.Module):
         bidirectional: bool = False,
         lstm_dropout: float = 0.2
     ) -> None:
-
         super(VisualOdometryModel, self).__init__()
 
         # Load pre-trained ResNet model
@@ -26,23 +23,33 @@ class VisualOdometryModel(nn.Module):
         for param in self.cnn_model.parameters():
             param.requires_grad = False
 
-        # TODO: create the LSTM
+        # LSTM layer
+        self.lstm = nn.LSTM(
+            input_size=resnet_output,
+            hidden_size=hidden_size,
+            num_layers=num_layers,
+            batch_first=True,
+            dropout=lstm_dropout,
+            bidirectional=bidirectional
+        )
 
-        # TODO: create the FC to generate the translation (3) and rotation (4)
+        # Fully connected layers to generate translation (3) and rotation (4)
+        self.fc = nn.Linear(hidden_size * (2 if bidirectional else 1), 7)
 
     def resnet_transforms(self) -> Callable:
         return weights.DEFAULT.transforms(antialias=True)
 
-    def forward(self, x: torch.TensorType) -> torch.TensorType:
-
-        # CNN feature extraction
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
         batch_size, seq_length, channels, height, width = x.size()
-        features = x.view(batch_size * seq_length, channels, height, width)
+        x = x.view(batch_size * seq_length, channels, height, width)
 
         with torch.no_grad():
-            features = self.cnn_model(features)
+            features = self.cnn_model(x)
 
-        # TODO: use the LSTM
+        features = features.view(batch_size, seq_length, -1)
 
-        # TODO: Get the output of the last time step
-        return ...
+        lstm_out, _ = self.lstm(features)
+
+        outputs = self.fc(lstm_out[:, -1, :])
+        return outputs
+
